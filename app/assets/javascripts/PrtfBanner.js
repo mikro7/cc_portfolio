d25d0fb67e689a154082968798c8e0504021460c
Mirko <mikromal@gmail.com>
2015-09-22 04:53:36 -0400

    Adding Blog page

--- app/assets/javascripts/PrtfBanner.js
+++ app/assets/javascripts/PrtfBanner.js
@@ -1,3 +1,3 @@
-$(window).load(function() {
-         $('#featured').orbit();
-     });
+$(function() {
+    $('.banner').unslider();
+});
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,15 @@
 class PagesController < ApplicationController
  
   def welcome
-	
 	end
   
   def portfolio
-    
   end
+  
+  def about
+  end
+  
+  def contact 
+  end
+  
 end
